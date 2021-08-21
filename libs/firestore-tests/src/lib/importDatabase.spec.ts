c3219e9009ab6e075ae25c45f096a95ff56339aa
tim <tja2353@gmail.com>
2021-08-21 17:18:26 +0100

    update importDatabase.spec.ts

--- libs/firestore-tests/src/lib/importDatabase.spec.ts
+++ libs/firestore-tests/src/lib/importDatabase.spec.ts
@@ -3,21 +3,7 @@
  *
  * Required for Firebase
  */
-import {
-  Collection,
-  importDatabase,
-  removeCollections,
-} from './importDatabase';
-
-type DocumentD = {
-    D_field1: string;
-    D_field2: number;
-  };
-
-type DocumentC = {
-  doc_C_field1: string;
-  doc_C_field2: number;
-};
+import { Collection, importDatabase } from './importDatabase';
 
 type DocumentA = {
   doc_A_field1: string;
@@ -26,64 +12,157 @@ type DocumentA = {
   doc_A_collectionD: Collection<DocumentD>;
 };
 
-// ## DocumentC1 ##
-const DocumentC1: DocumentC = {
-  doc_C_field1: 'docC_field1_1',
-  doc_C_field2: 6,
+type DocumentB = {
+  doc_B_field1: string;
+  doc_B_field2: number;
 };
 
-const expectedDocumentC1 = removeCollections(DocumentC1);
-
-const DocumentC1Path = 'collectionA/documentA1/doc_A_collectionC/documentC1';
-
-// ## DocumentA1 ##
-const DocumentA1: DocumentA = {
-  doc_A_field1: 'field1-A1',
-  doc_A_field2: 1,
-  doc_A_collectionC: { documentC1: DocumentC1 },
-  doc_A_collectionD: {},
+type DocumentC = {
+  doc_C_field1: string;
+  doc_C_field2: number;
 };
 
-const expectedDocumentA1 = removeCollections(DocumentA1);
-
-const DocumentA1Path = 'collectionA/documentA1';
-
-// ## DocumentA2 ##
-const DocumentA2: DocumentA = {
-  doc_A_field1: 'field1-A2',
-  doc_A_field2: 2,
-  doc_A_collectionC: {},
-  doc_A_collectionD: {},
+type DocumentD = {
+  doc_D_field1: string;
+  doc_D_field2: number;
 };
 
-const expectedDocumentA2 = removeCollections(DocumentA2);
+type Database = {
+  collectionA: Collection<DocumentA>;
+  collectionB: Collection<DocumentB>;
+};
 
-const DocumentA2Path = 'collectionA/documentA2';
-
-// ## DatabaseData ##
-const DatabaseData = {
+const Database: Database = {
   collectionA: {
-    documentA1: DocumentA1,
-    documentA2: DocumentA2,
+    documentA1: {
+      doc_A_field1: 'field1-A1',
+      doc_A_field2: 2,
+      doc_A_collectionC: {
+        documentC1: {
+          doc_C_field1: 'docC_field1-C1',
+          doc_C_field2: 6,
+        },
+        documentC2: {
+          doc_C_field1: 'docC_field1-C2',
+          doc_C_field2: 6,
+        },
+      },
+      doc_A_collectionD: {
+        documentD1: {
+          doc_D_field1: 'docD_field1-D1',
+          doc_D_field2: 6,
+        },
+      },
+    },
+    documentA2: {
+      doc_A_field1: 'field1-A2',
+      doc_A_field2: 2,
+      doc_A_collectionC: {},
+      doc_A_collectionD: {},
+    },
+    documentA3: {
+      doc_A_field1: 'field1-A3',
+      doc_A_field2: 2,
+      doc_A_collectionC: {},
+      doc_A_collectionD: {},
+    },
+  },
+  collectionB: {
+    documentB1: {
+      doc_B_field1: 'field1-B1',
+      doc_B_field2: 2,
+    },
   },
 };
 
 describe('test1', () => {
-  it('1 collection with 2 documents', async () => {
+  it('test database', async () => {
     const mockSaveFn = jest.fn();
-    importDatabase(DatabaseData, mockSaveFn);
-    expect(mockSaveFn.mock.calls.length).toBe(3);
+    importDatabase(Database, mockSaveFn);
+    expect(mockSaveFn.mock.calls.length).toBe(7);
 
-    // call: 1
-    expect(mockSaveFn.mock.calls[0][0]).toBe(DocumentA1Path);
-    expect(mockSaveFn.mock.calls[0][1]).toStrictEqual(expectedDocumentA1);
+    {
+      const call = 0;
+      const path = 'collectionA/documentA1';
+      const expected: Omit<
+        DocumentA,
+        'doc_A_collectionC' | 'doc_A_collectionD'
+      > = {
+        doc_A_field1: 'field1-A1',
+        doc_A_field2: 2,
+      };
 
-    // call: 2
-    expect(mockSaveFn.mock.calls[1][0]).toBe(DocumentC1Path);
-    expect(mockSaveFn.mock.calls[1][1]).toStrictEqual(expectedDocumentC1);
+      expect(mockSaveFn.mock.calls[call][0]).toBe(path);
+      expect(mockSaveFn.mock.calls[call][1]).toStrictEqual(expected);
+    }
+    {
+      const call = 1;
+      const path = 'collectionA/documentA1/doc_A_collectionC/documentC1';
+      const expected: DocumentC = {
+        doc_C_field1: 'docC_field1-C1',
+        doc_C_field2: 6,
+      };
+      expect(mockSaveFn.mock.calls[call][0]).toBe(path);
+      expect(mockSaveFn.mock.calls[call][1]).toStrictEqual(expected);
+    }
+    {
+      const call = 2;
+      const path = 'collectionA/documentA1/doc_A_collectionC/documentC2';
+      const expected: DocumentC = {
+        doc_C_field1: 'docC_field1-C2',
+        doc_C_field2: 6,
+      };
+      expect(mockSaveFn.mock.calls[call][0]).toBe(path);
+      expect(mockSaveFn.mock.calls[call][1]).toStrictEqual(expected);
+    }
+    {
+      const call = 3;
+      const path = 'collectionA/documentA1/doc_A_collectionD/documentD1';
+      const expected: DocumentD = {
+        doc_D_field1: 'docD_field1-D1',
+        doc_D_field2: 6,
+      };
+      expect(mockSaveFn.mock.calls[call][0]).toBe(path);
+      expect(mockSaveFn.mock.calls[call][1]).toStrictEqual(expected);
+    }
+    {
+      const call = 4;
+      const path = 'collectionA/documentA2';
+      const expected: Omit<
+        DocumentA,
+        'doc_A_collectionC' | 'doc_A_collectionD'
+      > = {
+        doc_A_field1: 'field1-A2',
+        doc_A_field2: 2,
+      };
 
-    // call: 3
-    expect(mockSaveFn.mock.calls[2][0]).toBe(DocumentA2Path);
-    expect(mockSaveFn.mock.calls[2][1]).toStrictEqual(expectedDocumentA2);
+      expect(mockSaveFn.mock.calls[call][0]).toBe(path);
+      expect(mockSaveFn.mock.calls[call][1]).toStrictEqual(expected);
+    }
+    {
+      const call = 5;
+      const path = 'collectionA/documentA3';
+      const expected: Omit<
+        DocumentA,
+        'doc_A_collectionC' | 'doc_A_collectionD'
+      > = {
+        doc_A_field1: 'field1-A3',
+        doc_A_field2: 2,
+      };
+
+      expect(mockSaveFn.mock.calls[call][0]).toBe(path);
+      expect(mockSaveFn.mock.calls[call][1]).toStrictEqual(expected);
+    }
+    {
+      const call = 6;
+      const path = 'collectionB/documentB1';
+      const expected: DocumentB = {
+        doc_B_field1: 'field1-B1',
+        doc_B_field2: 2,
+      };
+
+      expect(mockSaveFn.mock.calls[call][0]).toBe(path);
+      expect(mockSaveFn.mock.calls[call][1]).toStrictEqual(expected);
+    }
   });
 });
